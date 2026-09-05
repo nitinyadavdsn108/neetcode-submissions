2c887bb03ece52a642b9fe4968c31efce28caed8
NITIN <102019082+nitinyadavdsn108@users.noreply.github.com>
2026-09-05 23:58:30 +0530

    Add: boats-to-save-people - submission-8

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-8.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+        int boatCount = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+
+            right--;
+            boatCount++;
+        }
+
+        return boatCount;
+    }
+}
